3d4fc2afbae5e0242a9e71a535222ca1951ed90d
gussfer <guolifer@gmail.com>
2022-09-14 13:39:50 -0300

    add-ex2.2

---
+++ exercises-lets-code-pass/ex2.2.py
@@ -0,0 +1,6 @@
+'''
+2.2) Faça um programa que peça para o usuário digitar uma palavra e imprima cada letra em uma linha.
+'''
+palavra = input("Digite uma palavra: ")
+for x in palavra:
+    print(x)
